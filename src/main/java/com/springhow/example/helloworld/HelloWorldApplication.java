25e288d21f480d568f0db28372a93cfb0c556c8c
harkiratmalhotra <40227521+harkiratmalhotra@users.noreply.github.com>
2023-04-05 14:58:15 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "WAR File Deployed to Test Environment through JenkinsFile!";
+        return "WAR File Deployed to Test Environment and on Prod Environment through JenkinsFile!";
     }
 
 }
